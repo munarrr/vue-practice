bfaa70595db9911701fb6d4feb20b83aaf01a297
Munar <karymshakovmunar@gmail.com>
2021-06-09 19:08:15 +0600

    @click

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+<h1>Проверь свою рекцию</h1>
+  <button @click="startGame" :disabled="isPlayng">Начать игру</button>
+  <div v-if="delay">
+    <Block :delay="delay"   @reactionTime="test"/>
+  </div>
+    <Result v-if="showResults" :score="score"/>
+  </div>
+</template>
+
+<script>
+import Block  from "./components/Block.vue";
+import Result from "./components/Result.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Result,
+    Block
+  },
+  data(){
+    return{
+      isPlayng:false,
+      showResults: false,
+      delay:null,
+      score:null,
+    };
+  },
+  methods: {
+    startGame() {
+      this.delay = Math.floor(Math.random() * 5000);
+    },
+    test(reactionTime) {
+     this.score= reactionTime;
+     this.showResults = true
+    }
+  }
+  }
+</script>
+
+<style>
+  #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+  button[disabled]{
+    opacity:0.3;
+    cursor: not-allowed
+  ;
+
+  }
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,46 @@
+<template>
+  <p>Ваше время реагирование {{score}}</p>
+  <p>{{rank}}</p>
+</template>
+
+<script>
+export default {
+  name: 'Result',
+  props:["score"],
+  data(){
+    return {
+      rank: null,
+    };
+    },
+    mounted(){
+      if (this.score < 250 && this.score >= 0) {
+        this.rank = 'Молниеносная Реакция'
+      }
+      if (this.score < 450 && this.score >= 250) {
+        this.rank = 'Шустрые пальцы, качаешься?'
+      }
+      if (this.score < 650 && this.score >= 450) {
+        this.rank = 'Неплохо, но, можно лучше'
+      }
+    }
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
